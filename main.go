0eb85a4b5f35bf41c8584e81a1c906a9690ddef6
Pascal Allen <pascal.allen88@gmail.com>
2024-10-04 13:58:54 -0400

    cleanup

--- main.go
+++ main.go
@@ -2,41 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-// OrganizeFiles moves files into directories based on their extensions
 func OrganizeFiles(directory string) {
-	// Read the contents of the directory
-	files, err := ioutil.ReadDir(directory)
+	files, err := os.ReadDir(directory)
 	if err != nil {
 		log.Fatalf("Error reading directory: %v", err)
 	}
 
-	// Iterate over each file in the directory
 	for _, file := range files {
-		// Skip if it's a directory
 		if file.IsDir() {
 			continue
 		}
 
-		// Get the file extension (lowercased)
 		ext := strings.ToLower(filepath.Ext(file.Name()))
 		if ext == "" {
-			continue // Skip files without an extension
+			continue
 		}
 
-		// Create a directory for the extension if it doesn't exist
-		extDir := filepath.Join(directory, ext[1:]) // Removing the leading dot from extension
+		extDir := filepath.Join(directory, ext[1:])
 		if _, err := os.Stat(extDir); os.IsNotExist(err) {
 			os.Mkdir(extDir, 0755)
 		}
 
-		// Move the file into the appropriate directory
 		oldPath := filepath.Join(directory, file.Name())
 		newPath := filepath.Join(extDir, file.Name())
 		if err := os.Rename(oldPath, newPath); err != nil {
@@ -48,19 +40,15 @@ func OrganizeFiles(directory string) {
 }
 
 func main() {
-	// Check if the user provided a directory path
 	if len(os.Args) < 2 {
 		log.Fatalf("Please provide a directory to organize. Usage: %s <directory>", os.Args[0])
 	}
 
-	// Get the directory from the command line arguments
 	directory := os.Args[1]
 
-	// Check if the directory exists
 	if _, err := os.Stat(directory); os.IsNotExist(err) {
 		log.Fatalf("Directory does not exist: %s", directory)
 	}
 
-	// Organize files in the specified directory
 	OrganizeFiles(directory)
 }
